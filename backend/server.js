d1139317293864b8b4d6676ffbeaa81ee6ecae97
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-17 10:02:25 -0400

    I built my fullstack MERNSTACK chat app

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,9 @@
-import express from "express";
+const express = require("express");
 
 const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
 app.listen(5000, () => console.log("Server Running on port 5000"));
