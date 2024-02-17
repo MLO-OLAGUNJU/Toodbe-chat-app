144de7eda2e66561a34dba66e9489d37d3c0d9d4
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-17 11:08:09 -0400

    I built my fullstack MERNSTACK chat app

---
+++ backend/controllers/auth.controllers.js
@@ -0,0 +1,10 @@
+export const signup = (req, res) => {
+  res.send("singn");
+  console.log("signUpUser");
+};
+export const login = (req, res) => {
+  console.log("loginUser");
+};
+export const logout = (req, res) => {
+  console.log("logoutUser");
+};
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
+import { login, logout, signup } from "../controllers/auth.controllers.js";
 
 const router = express.Router();
 
-router.get("/signup");
-router.get("/login");
-router.get("/logout");
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
 
 export default router;
