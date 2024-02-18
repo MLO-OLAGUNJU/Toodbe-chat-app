ec0bc559d7b32eeccbce1210a04af044e130750a
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-18 12:00:57 -0400

    I built my fullstack MERNSTACK chat app

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -90,5 +90,14 @@ export const login = async (req, res) => {
   }
 };
 export const logout = (req, res) => {
-  console.log("logoutUser");
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "User logged out" });
+  } catch (error) {
+    res.status(500).json({
+      error: "Internal Server Error",
+    });
+
+    console.log("Error in logout controller", error.message);
+  }
 };
