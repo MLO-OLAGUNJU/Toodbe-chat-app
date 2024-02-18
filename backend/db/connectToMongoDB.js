51beacb01c6211988909bbb7a2caede0a7672bf4
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-18 08:38:17 -0400

    I built my fullstack MERNSTACK chat app

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -3,7 +3,7 @@ import { mongoose } from "mongoose";
 const connectToMongoDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_DB_URI);
-    console.log("running");
+    console.log("Coonected to MongoDB");
   } catch (error) {
     console.log("Error connecting", error.message);
   }
