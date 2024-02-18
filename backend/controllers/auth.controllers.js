c0cd822faa03e75c1a0b4f7852ea05c7b424b510
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-18 10:23:37 -0400

    I built my fullstack MERNSTACK chat app

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -12,7 +12,26 @@ export const signup = async (req, res) => {
 
     const user = await User.findOne({ username });
     if (user) {
+      return res.status(400).json({
+        error: "Username already exists",
+      });
     }
+
+    //HASH PASSWORD HERE
+    //https://avatar-placeholder.iran.liara.run/
+
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+    const newUser = new User({
+      fullName,
+      username,
+      password,
+      gender,
+      profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+    });
+
+    await newUser.save();
   } catch (error) {}
 };
 export const login = (req, res) => {
