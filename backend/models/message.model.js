cee724035d85028b33259ff43b8f316b953ce14b
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-18 12:19:48 -0400

    I built my fullstack MERNSTACK chat app

--- backend/models/message.model.js
+++ backend/models/message.model.js
@@ -3,5 +3,11 @@ const messageSchema = new mongoose.Schema({
   senderId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
+    required: true,
+  },
+  receiverId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
   },
 });
