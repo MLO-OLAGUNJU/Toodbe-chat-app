a5284a0499655aa692ca2eb37d35ec1b4838d80e
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-18 09:18:36 -0400

    I built my fullstack MERNSTACK chat app

---
+++ backend/models/user.model.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+});
