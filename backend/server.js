be02963b198419384ce5e66f6daa5fb798a6b3e6
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-18 09:44:41 -0400

    I built my fullstack MERNSTACK chat app

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
-const app = express();
-
 import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
-dotenv.config();
+const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!!");
-});
+dotenv.config();
 
 app.use("/api/auth", authRoutes);
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World!!");
+// });
+
 app.listen(PORT, () => {
   connectToMongoDB();
   console.log("Server Running on port " + PORT);
