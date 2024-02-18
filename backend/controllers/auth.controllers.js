2d7bae929cb07599ce4cbee14877838da7b13a76
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-18 11:29:09 -0400

    I built my fullstack MERNSTACK chat app

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
 
 export const signup = async (req, res) => {
   try {
@@ -20,7 +21,7 @@ export const signup = async (req, res) => {
 
     //HASH PASSWORD HERE
     const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hashPassword(password, salt);
+    const hashedPassword = await bcrypt.hash(password, salt);
     //https://avatar-placeholder.iran.liara.run/
 
     const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
@@ -34,14 +35,21 @@ export const signup = async (req, res) => {
       profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
     });
 
-    await newUser.save();
+    if (newUser) {
+      await generateTokenAndSetCookie();
+      await newUser.save();
 
-    res.status(201).json({
-      _id: newUser._id,
-      fullName: newUser.fullName,
-      username: newUser.username,
-      profilePic: newUser.profilePic,
-    });
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({
+        error: "Invalid user data",
+      });
+    }
   } catch (error) {
     res.status(500).json({
       error: "Internal Server Error",
---
+++ backend/utils/generateToken.js
@@ -0,0 +1,15 @@
+import jwt from "jsonwebtoken";
+
+const generateTokenAndSetCookie = (userId, res) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "15d",
+  });
+
+  res.cookie("jwt", token, {
+    nmaxAge: 15 * 24 * 60 * 60 * 1000, //max age of cookie in milliseconds
+    httpOnly: true, //prevent XSS attacks cross-site scripting attacks
+    sameSite: "strict", ///CSRF attacks cross-site request forgery attacks
+  });
+};
+
+export default generateTokenAndSetCookie;
