bab400608f6736b4921452c7e91d02aafce07f4b
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-19 16:54:48 +0100

    Building Toodbe Chat App Frontend

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -3,13 +3,13 @@ import React from "react";
 const Login = () => {
   return (
     <>
-      <main className="w-[80%] h-[80%] flex items-center justify-center mx-auto">
+      <main className="w-[80%] h-[80%] flex items-center justify-between mx-auto">
         <div>
           <img className="w-full" src="/Toodbe-nobg.png" alt="Toodbe Logo" />
         </div>
-        <div className="flex flex-col items-center justify-center min-w-96 mx-auto ">
-          <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
-            <h1 className="text-5xl font-semibold text-center text-gray-300">
+        <div className="flex flex-col items-center justify-center min-w-96 ">
+          <div className="w-full p-6 rounded-lg shadow-md bg-[#F5F5F5] bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+            <h1 className=" text-5xl font-semibold text-center text-gray-300">
               Sign In
             </h1>
 
@@ -18,6 +18,11 @@ const Login = () => {
                 <label className="label p-2">
                   <span className="text-base label-text">Username</span>
                 </label>
+                <input
+                  type="text"
+                  placeholder="Enter Username"
+                  className="w-full input input-bordered h-10"
+                />
               </div>
             </form>
           </div>
