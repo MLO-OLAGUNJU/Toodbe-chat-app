070272e47740fbd30324dec6b746015b5180ae30
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-19 15:20:24 +0100

    I built my fullstack MERNSTACK chat app

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -4,7 +4,9 @@ export const getUsersForSideBar = async (req, res) => {
   try {
     const loggedInUserId = req.user._id;
 
-    const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } });
+    const filteredUsers = await User.find({
+      _id: { $ne: loggedInUserId },
+    }).select("-password");
 
     res.status(200).json(filteredUsers);
   } catch (error) {
