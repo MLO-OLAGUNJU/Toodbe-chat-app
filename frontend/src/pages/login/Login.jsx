007e8cd03d700c5a3ebbcd017154a44f5436ba79
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-19 17:39:17 +0100

    Building Toodbe Chat App Frontend

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -8,32 +8,49 @@ const Login = () => {
           <img className="w-full" src="/Toodbe-nobg.png" alt="Toodbe Logo" />
         </div>
         <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
-          <div className="w-full p-6 border-[#1D9BF0] border-2 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+          <div className="w-full p-12 border-blue-600 border-2 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
             <h1 className=" text-5xl font-semibold text-center text-gray-300">
-              Sign In
+              Sign In <span className=" text-blue-600">ToodBE</span>
             </h1>
 
             <form>
               <div>
-                <label className="label p-2">
-                  <span className="text-base label-text">Username</span>
+                <label className="label p-2 ">
+                  <span className="text-base  text-blue-600 label-text">
+                    Username
+                  </span>
                 </label>
                 <input
                   type="text"
                   placeholder="Enter username"
-                  className="w-[500px] input input-bordered h-10"
+                  className="w-[500px] input input-bordered h-14 focus:border-blue-600"
                 />
               </div>
 
               <div>
                 <label className="label p-2">
-                  <span className="text-base label-text">Password</span>
+                  <span className="text-base  text-blue-600 label-text">
+                    Password
+                  </span>
                 </label>
                 <input
                   type="password"
                   placeholder="Enter password"
-                  className="w-[500px] input input-bordered h-10"
+                  className="w-[500px] input input-bordered h-14 focus:border-blue-600"
                 />
+              </div>
+
+              <a
+                href="#"
+                className="text-sm  hover:underline hover:text-blue-600 mt-2 inline-block"
+              >
+                Don't have an account?
+              </a>
+
+              <div>
+                <button className="btn btn-block btn-sm mt-2 bg-blue-600">
+                  Sign In
+                </button>
               </div>
             </form>
           </div>
