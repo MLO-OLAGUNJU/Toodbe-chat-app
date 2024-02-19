efff6f28a382a74a85b3aebb9add40453d5d5c23
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-19 17:18:00 +0100

    Building Toodbe Chat App Frontend

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -8,7 +8,7 @@ const Login = () => {
           <img className="w-full" src="/Toodbe-nobg.png" alt="Toodbe Logo" />
         </div>
         <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
-          <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+          <div className="w-full p-6 border-[#1D9BF0] border-2 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
             <h1 className=" text-5xl font-semibold text-center text-gray-300">
               Sign In
             </h1>
@@ -21,6 +21,17 @@ const Login = () => {
                 <input
                   type="text"
                   placeholder="Enter username"
+                  className="w-[500px] input input-bordered h-10"
+                />
+              </div>
+
+              <div>
+                <label className="label p-2">
+                  <span className="text-base label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="Enter password"
                   className="w-[500px] input input-bordered h-10"
                 />
               </div>
