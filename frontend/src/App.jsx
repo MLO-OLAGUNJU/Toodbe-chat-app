f89331cc82a9b4b54e329603284aab57bf11d279
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-19 18:13:54 +0100

    Building Toodbe Chat App Frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
 import Login from "./pages/login/Login";
+import SignUp from "./pages/signup/SignUp";
 
 function App() {
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      <Login />
+      <SignUp />
     </div>
   );
 }
--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const SignUp = () => {
-  return <div>signUp</div>;
+  return (
+    <>
+      <main className="w-[80%] h-[80%] flex items-center justify-between mx-auto">
+        <div>
+          <img className="w-full" src="/Toodbe-nobg.png" alt="Toodbe Logo" />
+        </div>
+        <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+          <div className="w-full p-12 border-blue-600 border-2 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0"></div>
+        </div>
+      </main>
+    </>
+  );
 };
 
 export default SignUp;
