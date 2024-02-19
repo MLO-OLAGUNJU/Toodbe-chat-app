58e8eeb586910a6ab90392605599646be823f819
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-19 16:45:48 +0100

    Building Toodbe Chat App Frontend

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <>
+      <main className="w-[80%] h-[80%] flex items-center justify-center mx-auto">
+        <div>
+          <img className="w-full" src="/Toodbe-nobg.png" alt="Toodbe Logo" />
+        </div>
+        <div className="flex flex-col items-center justify-center min-w-96 mx-auto ">
+          <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+            <h1 className="text-5xl font-semibold text-center text-gray-300">
+              Sign In
+            </h1>
+
+            <form>
+              <div>
+                <label className="label p-2">
+                  <span className="text-base label-text">Username</span>
+                </label>
+              </div>
+            </form>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 };
 
 export default Login;
