db2c94a5918b68fa72db2e3c590e7df722016c0b
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-19 20:20:10 +0100

    Building Toodbe Chat App Frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import SignUp from "./pages/signup/SignUp";
 function App() {
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      <Login />
+      <SignUp />
     </div>
   );
 }
