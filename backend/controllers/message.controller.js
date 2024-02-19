6f75d27eb58fb21dc301cd1bbd15cfad0250dc76
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-19 14:58:29 +0100

    I built my fullstack MERNSTACK chat app

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,10 +1,11 @@
 import Conversation from "../models/conversation.model.js";
+import Message from "../models/message.model.js";
 
-export const messageRoutes = async (req, res) => {
+export const sendMessages = async (req, res) => {
   try {
     const { message } = req.body;
     const { id: receiverId } = req.params;
-    const { senderId } = req.user._id;
+    const senderId = req.user._id;
 
     let conversation = await Conversation.findOne({
       participants: { $all: [senderId, receiverId] },
@@ -17,19 +18,43 @@ export const messageRoutes = async (req, res) => {
     }
 
     const newMessage = new Message({
-      senderId: senderId,
-      receiverId: receiverId,
-      message: message,
+      senderId,
+      receiverId,
+      message,
     });
 
     if (newMessage) {
       conversation.messages.push(newMessage._id);
-      // conversation.save();
     }
+
+    //SOCKET.IO FUNCTIONALITY WILL GO HERE
+
+    // await conversation.save();
+    // await newMessage.save();
+
+    //or
+    //this will run in parallel
+
+    await Promise.all([conversation.save(), conversation.save()]);
+
     res.status(201).json(newMessage);
-    // throw Error("Could not save the message to database");
   } catch (error) {
     console.log("Error in sendMessage contoller: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const senderId = req.user._id;
+    const conversation = await Conversation.findOne({
+      participants: { $all: [senderId, userToChatId] },
+    }).populate("messages");
+
+    res.status(200).json(conversation.messages);
+  } catch (error) {
+    console.log("Error in getMessages contoller: ", error.message);
     res.status(500).json({ error: "Internal server error" });
   }
 };
--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { messageRoutes } from "../controllers/message.controller.js";
+import {
+  getMessages,
+  sendMessages,
+} from "../controllers/message.controller.js";
 import protectRoute from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
-router.post("/send/:id", protectRoute, messageRoutes);
-// router.post("/login", login);
-// router.post("/logout", logout);
+router.get("/:id", protectRoute, getMessages);
+router.post("/send/:id", protectRoute, sendMessages);
 
 export default router;
