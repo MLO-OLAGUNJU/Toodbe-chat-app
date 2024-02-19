94aa96e1f02593d0cc01925c146d69da50fc2ba9
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-19 20:25:48 +0100

    Building Toodbe Chat App Frontend

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -10,7 +10,8 @@ const SignUp = () => {
         <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
           <div className="w-full p-12 border-blue-600 border-2 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
             <h1 className=" max-[480px]:text-3xl text-5xl font-semibold text-center text-gray-300">
-              Join <span className=" text-blue-600">ToodBE</span> Today.
+              Join <span className=" text-blue-600">ToodBE</span>
+              <span className="max-[480px]:hidden"> Today.</span>
             </h1>
 
             <form>
