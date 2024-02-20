0a428735840868a89d4663b8a40a79f7715e569f
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-20 12:39:35 +0100

    Building Toodbe Chat App Frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,9 @@ import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      <Routes></Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
