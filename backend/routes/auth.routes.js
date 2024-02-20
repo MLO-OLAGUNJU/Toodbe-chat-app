acdf81c0407578214ecae235ff842ea19a6e5250
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-20 15:45:14 +0100

    modified ToodBe backend

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -3,7 +3,7 @@ import { login, logout, signup } from "../controllers/auth.controllers.js";
 
 const router = express.Router();
 
-router.post("/signup", signup);
+router.post("/join", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 
