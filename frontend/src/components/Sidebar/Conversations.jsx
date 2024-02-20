10f88dbe2d66b1c611b37e28f42b7ce47d585ade
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-20 23:27:41 +0100

    Building Toodbe Chat App Frontend

--- frontend/src/components/Sidebar/Conversations.jsx
+++ frontend/src/components/Sidebar/Conversations.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import Conversation from "./Conversation";
+import useGetConversations from "../../hooks/useGetConversations";
 
 const Conversations = () => {
+  const { loading, conversations } = useGetConversations();
+  console.log("CONVERSATIONS", conversations);
   return (
     <div className="py-2 flex flex-col overflow-auto">
       <Conversation />
