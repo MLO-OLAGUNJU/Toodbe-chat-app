8dc3daa0b69b9e5dc3d41efe4192abef5392e8c9
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-20 12:39:03 +0100

    Building Toodbe Chat App Frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,12 @@ import "./App.css";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import SignUp from "./pages/signup/SignUp";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      <Home />
+      <Routes></Routes>
     </div>
   );
 }
