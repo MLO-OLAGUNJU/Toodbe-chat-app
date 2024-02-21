e11cb32a8160b9f648482b7bebf2b345a7f2ef73
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-21 12:11:51 +0100

    Building Toodbe Chat App Frontend

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -5,27 +5,7 @@ import React from "react";
 const Messages = () => {
   const { messages, loading } = useGetMessages();
   console.log("messages:", messages);
-  return (
-    <div className="flex-1 px-4 overflow-auto">
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-    </div>
-  );
+  return <div className="flex-1 px-4 overflow-auto"></div>;
 };
 
 export default Messages;
