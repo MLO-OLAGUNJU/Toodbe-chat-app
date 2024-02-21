5cf6e89bd43dba8de100eb10fc73bb9cb7f543d9
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-21 12:57:19 +0100

    Building Toodbe Chat App Frontend

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,19 +1,27 @@
 import React from "react";
+import useAuthContext from "../../context/AuthContext";
+import useConversation from "../../zustand/useConversation";
 
-const Message = () => {
+const Message = ({ message }) => {
+  const { authUser } = useAuthContext();
+  const { selectedConversation } = useConversation();
+  const fromMe = message.senderId === authUser._id;
+  const chatClassname = fromMe ? "chat-end" : "chat-start";
+  const profilePic = fromMe
+    ? authUser.profilePic
+    : selectedConversation?.profilePic;
+
+  const bubbleBgColor = fromMe ? "bg-blue-600" : "";
   return (
-    <div className="chat chat-end">
-      <div className="chat-image avatar">
+    <div className={`chat ${chatClassname}`}>
+      <div className={`chat-image avatar`}>
         <div className="w-10 rounded-full">
-          <img
-            src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-            alt=""
-          />
+          <img src={profilePic} alt="" />
         </div>
       </div>
 
-      <div className="chat-bubble text-white bg-blue-600">
-        Hi! What is your upp?
+      <div className={`chat-bubble text-white bg-blue-600 ${bubbleBgColor}`}>
+        {message.message}
       </div>
       <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">
         12:42
--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,11 +1,31 @@
 import useGetMessages from "../../hooks/useGetMessages";
+import MessageSkeleton from "../skeletons/MessageSkeleton";
 import Message from "./Message";
 import React from "react";
+import useConversation from "../../zustand/useConversation";
 
 const Messages = () => {
+  const { selectedConversation, setSelectedConversation } = useConversation();
+
   const { messages, loading } = useGetMessages();
   console.log("messages:", messages);
-  return <div className="flex-1 px-4 overflow-auto"></div>;
+  return (
+    <div className="flex-1 px-4 overflow-auto">
+      {!loading &&
+        messages.length > 0 &&
+        messages.map((message) => (
+          <Message key={message._id} message={message} />
+        ))}
+
+      {loading && [...Array(6)].map((_, idx) => <MessageSkeleton />)}
+
+      {!loading && messages.length === 0 && (
+        <p className="text-center">
+          Start a chat with {selectedConversation.fullName}
+        </p>
+      )}
+    </div>
+  );
 };
 
 export default Messages;
