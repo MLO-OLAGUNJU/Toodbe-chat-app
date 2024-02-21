33b1209de8ecd38988eaae4547b4f9b0a67c215e
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-21 19:43:48 +0100

    modified the backend

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,6 +1,6 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
-import { getReceiverSocketId } from "../socket/socket.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
 
 export const sendMessages = async (req, res) => {
   try {
