e480011e5de9113c3eb3be15077df4b5aeb0b407
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-21 14:58:05 +0100

    Building Toodbe Chat App Frontend

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,33 +1,36 @@
-import React from "react";
-import useAuthContext from "../../context/AuthContext";
+import { useAuthContext } from "../../context/AuthContext";
+import { extractTime } from "../../utils/extractTime";
 import useConversation from "../../zustand/useConversation";
 
 const Message = ({ message }) => {
   const { authUser } = useAuthContext();
   const { selectedConversation } = useConversation();
   const fromMe = message.senderId === authUser._id;
-  const chatClassname = fromMe ? "chat-end" : "chat-start";
+  const formattedTime = extractTime(message.createdAt);
+  const chatClassName = fromMe ? "chat-end" : "chat-start";
   const profilePic = fromMe
     ? authUser.profilePic
     : selectedConversation?.profilePic;
+  const bubbleBgColor = fromMe ? "bg-blue-500" : "";
 
-  const bubbleBgColor = fromMe ? "bg-blue-600" : "";
+  const shakeClass = message.shouldShake ? "shake" : "";
+
   return (
-    <div className={`chat ${chatClassname}`}>
-      <div className={`chat-image avatar`}>
+    <div className={`chat ${chatClassName}`}>
+      <div className="chat-image avatar">
         <div className="w-10 rounded-full">
-          <img src={profilePic} alt="" />
+          <img alt="Tailwind CSS chat bubble component" src={profilePic} />
         </div>
       </div>
-
-      <div className={`chat-bubble text-white bg-blue-600 ${bubbleBgColor}`}>
+      <div
+        className={`chat-bubble text-white ${bubbleBgColor} ${shakeClass} pb-2`}
+      >
         {message.message}
       </div>
       <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">
-        12:42
+        {formattedTime}
       </div>
     </div>
   );
 };
-
 export default Message;
