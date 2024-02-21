bc6a453b266e66c526a5876e29525b8f74379fb5
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-21 02:04:31 +0100

    Building Toodbe Chat App Frontend

--- frontend/src/components/Sidebar/Conversation.jsx
+++ frontend/src/components/Sidebar/Conversation.jsx
@@ -1,25 +1,31 @@
 import React from "react";
+import useConversation from "../../zustand/useConversation";
 
-const Conversation = () => {
+const Conversation = ({ conversation, lastIdx }) => {
+  const { selectedConversation, setSelectedConversation } = useConversation();
+
+  const isSelected = selectedConversation?._id === conversation._id;
   return (
     <>
-      <div className="flex gap-2 items-center hover:bg-blue-600 rounded p-2 py-1 cursor-pointer">
+      <div
+        className={`flex gap-2 items-center hover:bg-blue-600 rounded p-2 py-1 cursor-pointer
+    ${isSelected ? "bg-blue-600" : ""}`}
+        onClick={() => setSelectedConversation(conversation)}
+      >
         <div className="avatar online">
           <div className="w-12 rounded-full">
-            <img
-              src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-              alt="user avatar"
-            />
+            <img src={conversation.profilePic} alt="user avatar" />
           </div>
         </div>
 
         <div className="flex flex-col flex-1">
           <div className="flex gap-3 justify-between">
-            <p className=" font-bold text-gray-200">Sponge Kroos</p>
+            <p className=" font-bold text-gray-200">{conversation.fullName}</p>
           </div>
         </div>
       </div>
-      <div className="divider my-0 py-0 h-1"></div>
+
+      {!lastIdx && <div className="divider my-0 py-0 h-1"></div>}
     </>
   );
 };
--- frontend/src/components/Sidebar/LogoutButton.jsx
+++ frontend/src/components/Sidebar/LogoutButton.jsx
@@ -5,7 +5,7 @@ import useLogout from "../../hooks/useLogout";
 const LogoutButton = () => {
   const { loading, logout } = useLogout();
   return (
-    <div className="mt-auto">
+    <div className=" mt-8">
       {!loading ? (
         <BiLogOut
           className="w-6 h-6 text-white cursor-pointer"
