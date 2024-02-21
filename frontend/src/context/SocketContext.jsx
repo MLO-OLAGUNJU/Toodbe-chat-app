eaa812523b852aa60b57ab9651f0cf77cd6a0eba
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-21 16:52:37 +0100

    Building Toodbe Chat App Frontend

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -1,12 +1,32 @@
 import React, { useEffect, useState } from "react";
 import { createContext } from "react";
+import { useAuthContext } from "./AuthContext";
+import io from "socket.io-client";
 
 export const SocketContext = createContext();
 
 export const SocketContextProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
   const [onlineUsers, setOnlineUsers] = useState([]);
+  const { authUser } = useAuthContext();
 
-  useEffect(() => {}, []);
-  return <SocketContext.Provider value={{}}>{children}</SocketContext.Provider>;
+  useEffect(() => {
+    if (authUser) {
+      const socket = io("http://localhost:5000");
+
+      setSocket(socket);
+
+      return () => socket.close();
+    } else {
+      if (socket) {
+        socket.close();
+        setSocket(null);
+      }
+    }
+  }, []);
+  return (
+    <SocketContext.Provider value={{ socket, onlineUsers }}>
+      {children}
+    </SocketContext.Provider>
+  );
 };
