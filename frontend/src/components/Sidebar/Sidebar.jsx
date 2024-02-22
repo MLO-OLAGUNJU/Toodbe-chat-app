f3a9ebc5815dbcd264ec885321f72fbd27f9d163
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-22 15:48:10 +0100

    updating the app(responsiveness)

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-/* import React from "react";
+import React from "react";
 import SearchInput from "./SearchInput";
 import Conversations from "./Conversations";
 import LogoutButton from "./LogoutButton";
@@ -6,30 +6,6 @@ import LogoutButton from "./LogoutButton";
 const Sidebar = () => {
   return (
     <div className="border-r border-blue-600 p-4 flex flex-col">
-      <SearchInput />
-      <div className="divider px-3"></div>
-      <Conversations />
-
-      <LogoutButton />
-    </div>
-  );
-};
-
-export default Sidebar;
- */
-
-import React from "react";
-import SearchInput from "./SearchInput";
-import Conversations from "./Conversations";
-import LogoutButton from "./LogoutButton";
-
-const Sidebar = ({ show }) => {
-  return (
-    <div
-      className={`border-r border-blue-600 p-4 flex flex-col ${
-        !show && "hide-on-mobile"
-      }`}
-    >
       <SearchInput />
       <div className="divider px-3"></div>
       <Conversations />
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-/* import React from "react";
+import React from "react";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import MessageContainer from "../../components/messages/MessageContainer";
 
@@ -6,32 +6,8 @@ const Home = () => {
   return (
     <>
       <div className="flex w-full h-full rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
-        <Sidebar />
-        <MessageContainer />
-      </div>
-    </>
-  );
-};
-
-export default Home;
- */
-
-import React, { useState } from "react";
-import Sidebar from "../../components/Sidebar/Sidebar";
-import MessageContainer from "../../components/messages/MessageContainer";
-
-const Home = () => {
-  const [showSidebar, setShowSidebar] = useState(true);
-
-  const toggleSidebar = () => {
-    setShowSidebar((prev) => !prev);
-  };
-
-  return (
-    <>
-      <div className="flex w-full h-full rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
-        <Sidebar show={showSidebar} />
-        <MessageContainer toggleSidebar={toggleSidebar} />
+        <Sidebar className="leftside" />
+        <MessageContainer className="rightside" />
       </div>
     </>
   );
