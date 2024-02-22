ff9147ff0a1b0dd6d8da73529118e08970467f24
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-23 00:31:32 +0100

    updated the mobile responsiveness

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -27,7 +27,7 @@ const MessageContainer = ({ showChat, handleToggle }) => {
         <>
           {/* Header */}
           <div className="bg-blue-600 px-4 flex items-center gap-3 py-2 mb-2">
-            <FaLeftLong onClick={handleToggle} className="min-[768px]:hidden" />
+            <FaLeftLong onClick={handleToggle} className="lg:hidden" />
             <span className="label-text">To:</span>{" "}
             <span className="text-gray-900 font-bold">
               {selectedConversation.fullName}
