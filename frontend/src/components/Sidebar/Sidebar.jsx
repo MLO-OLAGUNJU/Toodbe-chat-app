fdb1e2996f191a1073f7649a656c8341f75250b2
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-22 17:16:17 +0100

    updating the app(responsiveness)

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -5,15 +5,13 @@ import LogoutButton from "./LogoutButton";
 
 const Sidebar = () => {
   return (
-    <main className="leftside">
-      <div className="border-r border-blue-600 p-4 flex flex-col">
-        <SearchInput />
-        <div className="divider px-3"></div>
-        <Conversations />
+    <div className="leftside border-r border-blue-600 p-4 flex flex-col">
+      <SearchInput />
+      <div className="divider px-3"></div>
+      <Conversations />
 
-        <LogoutButton />
-      </div>
-    </main>
+      <LogoutButton />
+    </div>
   );
 };
 
--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -13,26 +13,24 @@ const MessageContainer = () => {
   }, [setSelectedConversation]);
 
   return (
-    <main className="rightside">
-      <div className=" w-[100%] flex flex-col">
-        {!selectedConversation ? (
-          <NoChatSelected />
-        ) : (
-          <>
-            {/* Header */}
-            <div className="bg-blue-600 px-4 py-2 mb-2">
-              <span className="label-text">To:</span>{" "}
-              <span className="text-gray-900 font-bold">
-                {selectedConversation.fullName}
-              </span>
-            </div>
+    <div className="rightside w-[100%] flex flex-col">
+      {!selectedConversation ? (
+        <NoChatSelected />
+      ) : (
+        <>
+          {/* Header */}
+          <div className="bg-blue-600 px-4 py-2 mb-2">
+            <span className="label-text">To:</span>{" "}
+            <span className="text-gray-900 font-bold">
+              {selectedConversation.fullName}
+            </span>
+          </div>
 
-            <Messages />
-            <MessageInput />
-          </>
-        )}
-      </div>
-    </main>
+          <Messages />
+          <MessageInput />
+        </>
+      )}
+    </div>
   );
 };
 
