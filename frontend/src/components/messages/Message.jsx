1828e53f7a8a7f85ec4cf9bf083145aa007184ee
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-23 02:00:56 +0100

    updated the mobile responsiveness

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -29,7 +29,9 @@ const Message = ({ message }) => {
         {message.message}
       </div>
       <div
-        className={`self-end ml-1 ${message.seen ? "#1d232a" : ""} font-bold`}
+        className={`self-end ml-1 ${
+          message.seen ? "text-[#1d232a]" : ""
+        } font-bold`}
       >
         <BsCheck2All className="w-4 h-4" />
       </div>
