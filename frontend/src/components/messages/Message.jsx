b2e61989c9cd12b454cc9736e1e499cf4a3c5151
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-23 01:42:11 +0100

    updated the mobile responsiveness

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,8 +1,8 @@
 import { useAuthContext } from "../../context/AuthContext";
 import { extractTime } from "../../utils/extractTime";
 import useConversation from "../../zustand/useConversation";
-import { BsCheck2All } from "react-icons/bs";
-import { Box } from "@chakra-ui/react";
+// import { BsCheck2All } from "react-icons/bs";
+// import { Box } from "@chakra-ui/react";
 
 const Message = ({ message }) => {
   const { authUser } = useAuthContext();
@@ -29,14 +29,14 @@ const Message = ({ message }) => {
       >
         {message.message}
       </div>
-      <Box
+      {/* <Box
         alignSelf={"flex-end"}
         ml={1}
         color={message.seen ? "white" : "black"}
         fontWeight={"bold"}
       >
         <BsCheck2All size={16} />
-      </Box>
+      </Box> */}
       <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">
         {formattedTime}
       </div>
